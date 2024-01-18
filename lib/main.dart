666f63bc127ea4f82c52800d3cf52acf9899a00c
RAHMA199 <RAHMA199>
2024-01-18 17:38:14 +0200

    adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:recipe_app/firebase_options.dart';
 import 'package:recipe_app/pages/splash.page.dart';
 import 'package:recipe_app/services/prefrences.serviceds.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,6 +10,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
     PrefrencesService.prefs = await SharedPreferences.getInstance();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     if (PrefrencesService.prefs != null) {
       if (kDebugMode) {
         if (kDebugMode) {
